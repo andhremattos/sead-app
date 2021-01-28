4ef232934cbebabf58720a2c3abdbec084fedbc3
Carlos Andre <andhremattos@gmail.com>
2021-01-28 16:11:27 -0300

    novo

---
+++ app.py
@@ -0,0 +1,24 @@
+#importando bibliotecas
+import streamlit as st
+
+
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+
+
+import numpy as np
+import pandas as pd
+
+st.title('Meu primeiro App usando Streamlit')
+
+st.write("Temos a informação de um data frame:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+""" 
+# Meu primeiro app
+
+Este é um **Aplicativo**.
+""" 
